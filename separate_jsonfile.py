2e8588da3e050f4ea6317ceb645556f080545109
hyunchun <hyunchun@umich.edu>
2018-02-24 22:17:17 -0500

    separate

---
+++ separate_jsonfile.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from __future__ import division
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import string
+import sys
+
+def main():
+    filename = sys.argv[1]
+    inFile = open("%s" %(filename), "r")
+
+    category_list = []
+    count = 0
+    while(count <= 500):
+        count += 50
+        inFile_count = 0
+        outFile = open("%s" %(filename + str(count)), "w")
+        for line in inFile:
+            outFile.write(line)
+            inFile_count += 1
+            if inFile_count == 50:
+                break
+
+
+    inFile.close()
+    outFile.close()
+ # ---------------------- #
+if __name__ == "__main__":
+    main()
