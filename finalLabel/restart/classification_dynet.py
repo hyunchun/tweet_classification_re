bf62b66f7c514a354a77e45333ad6fd97cb010e9
hyunchun <hyunchun@umich.edu>
2018-03-17 19:11:56 -0400

    updated classifiation_dynet (lookup added)

--- finalLabel/restart/classification_dynet.py
+++ finalLabel/restart/classification_dynet.py
@@ -7,9 +7,9 @@ import operator
 import dynet as dy
 
 try: 
-	import json
+    import json
 except ImportError:
-	import simplejson as json
+    import simplejson as json
 
 from sklearn.svm import SVC, LinearSVC
 from sklearn.svm import LinearSVC
@@ -31,63 +31,63 @@ from sklearn import metrics
 
 
 def extract_from_json(inFile):
-	textSet = []
-	contentLabelSet = []
-	typeLabelSet = []
-	unique_content = []
-	unique_type = []
+    textSet = []
+    contentLabelSet = []
+    typeLabelSet = []
+    unique_content = []
+    unique_type = []
 
-	for line in inFile:
-		line = json.loads(line.strip())
-		textSet.append(line["text"])
-		contentLabelSet.append(int(line["content_label"]))
-		if int(line["content_label"]) not in unique_content:
-			unique_content.append(int(line["content_label"]))
-		typeLabelSet.append(int(line["type_label"]))
-		if int(line["type_label"]) not in unique_type:
-			unique_type.append(int(line["type_label"]))
+    for line in inFile:
+        line = json.loads(line.strip())
+        textSet.append(line["text"])
+        contentLabelSet.append(int(line["content_label"]))
+        if int(line["content_label"]) not in unique_content:
+            unique_content.append(int(line["content_label"]))
+        typeLabelSet.append(int(line["type_label"]))
+        if int(line["type_label"]) not in unique_type:
+            unique_type.append(int(line["type_label"]))
 
-	return textSet, contentLabelSet, typeLabelSet, unique_content, unique_type
+    return textSet, contentLabelSet, typeLabelSet, unique_content, unique_type
 
 def label_separator(label_to_separate, text_set, content_label_set):
-	majority, majority_label, minority, minority_label = []
-	if label_to_separate == "type":
-		count = 0
-		for line in text_set:
-			if content_label_set[count] == 0:
-				majority.append(text_set[count])
-				majority_label.append(content_label_set[count])
-			else: 
-				minority.append(text_set[count])
-				minority_label.append(content_label_set[count])
+    majority, majority_label, minority, minority_label = []
+    if label_to_separate == "type":
+        count = 0
+        for line in text_set:
+            if content_label_set[count] == 0:
+                majority.append(text_set[count])
+                majority_label.append(content_label_set[count])
+            else: 
+                minority.append(text_set[count])
+                minority_label.append(content_label_set[count])
 
-	return majority, majority_label, minority, minority_label
+    return majority, majority_label, minority, minority_label
 
 def extract_dictionary(dataset, word_dict):
     index = 0
     for line in dataset:
-    	line = line.split()
-    	for word in line:
-    		# just word itself 
-    		if word not in word_dict:
-    			word_dict[word] = index
-    			index += 1
-    		try: 
-	    		# lower capiticalization of the word
-	    		lower_word = str(word).lower()
-	    		if lower_word not in word_dict:
-	    			word_dict[lower_word] = index
-	    			index += 1
+        line = line.split()
+        for word in line:
+            # just word itself 
+            if word not in word_dict:
+                word_dict[word] = index
+                index += 1
+            try: 
+                # lower capiticalization of the word
+                lower_word = str(word).lower()
+                if lower_word not in word_dict:
+                    word_dict[lower_word] = index
+                    index += 1
 
-	    		# no punctuation 
-		        replace_punctuation = str(word).maketrans(string.punctuation, '')
-		        clean_word = str(word).translate(replace_punctuation)
+                # no punctuation 
+                replace_punctuation = str(word).maketrans(string.punctuation, '')
+                clean_word = str(word).translate(replace_punctuation)
 
-		        if clean_word not in word_dict:
-	                    word_dict[clean_word] = index
-	                    index += 1
-	        except:
-	        	continue
+                if clean_word not in word_dict:
+                        word_dict[clean_word] = index
+                        index += 1
+            except:
+                continue
 
     print("done with extract_dictionary")
 
@@ -102,38 +102,38 @@ def generate_feature_matrix(dataset, word_dict):
     train_dict = {}
     line_count = 0
     for line in dataset:
-    	line = line.split()
-    	for word in line:
-    	    # check if RT
-    	    if word == "RT":
-    		feature_matrix[line_count][number_of_words] += 1
-    		break
+        line = line.split()
+        for word in line:
+            # check if RT
+            if word == "RT":
+                feature_matrix[line_count][number_of_words] += 1
+                continue
 
-    	    # check if hashtag
-    	    if word[0] == "#":
-    		feature_matrix[line_count][number_of_words + 1] += 1
+            # check if hashtag
+            if word[0] == "#":
+                feature_matrix[line_count][number_of_words + 1] += 1
 
-    	    # check if mention
-    	    if word[0] == "@":
-    		feature_matrix[line_count][number_of_words + 2] += 1
+            # check if mention
+            if word[0] == "@":
+                feature_matrix[line_count][number_of_words + 2] += 1
 
-    	    # just word itself 
-    	    if word not in word_dict:
-    		feature_matrix[line_count][word_dict[word]] += 1
+            # just word itself 
+            if word in word_dict:
+                feature_matrix[line_count][word_dict[word]] += 1
 
-    	    try: 
-	    	# lower capiticalization of the word
-	    	lower_word = str(word).lower()
-	    	if lower_word not in word_dict:
-        	    feature_matrix[line_count][word_dict[lower_word]] += 1
-    		# no punctuation 
-	        replace_punctuation = str(word).maketrans(string.punctuation, '')
-	        clean_word = str(word).translate(replace_punctuation)
+            try: 
+                # lower capiticalization of the word
+                lower_word = str(word).lower()
+                if lower_word in word_dict:
+                    feature_matrix[line_count][word_dict[lower_word]] += 1
+                # no punctuation 
+                replace_punctuation = str(word).maketrans(string.punctuation, '')
+                clean_word = str(word).translate(replace_punctuation)
 
-		if clean_word not in word_dict:
-	            feature_matrix[line_count][word_dict[clean_word]] += 1
-	    except:
-	        continue
+                if clean_word in word_dict:
+                    feature_matrix[line_count][word_dict[clean_word]] += 1
+            except:
+                continue
 
         line_count += 1
 
@@ -152,7 +152,7 @@ def main():
     test_file = open("%s" %(sys.argv[2]))
     train_text_set, train_content_label_set, train_type_label_set, unique_content, unique_type = extract_from_json(train_file)
     test_text_set, test_content_label_set, test_type_label_set, _, _ = extract_from_json(test_file)
-	
+    
     # content
     # majority, majority_label, minority, minority_label = label_separator("type", train_text_set, train_content_label_set)
 
@@ -161,7 +161,7 @@ def main():
 
     #resampled_train_text_set = minority + majority_dwsampled
     #resampled_train_type_label_set = minority_label + majority_dwsampled_label
-	
+    
     word_dict = {}
     word_dict = extract_dictionary(train_text_set, word_dict)
     word_dict = extract_dictionary(test_text_set, word_dict)
@@ -177,7 +177,7 @@ def main():
     pBias2_content = para_collec.add_parameters((len(unique_content)), dy.ConstInitializer(0))
     pW2_type = para_collec.add_parameters((len(unique_type), 450), dy.NormalInitializer())
     pBias2_type = para_collec.add_parameters((len(unique_type)), dy.ConstInitializer(0))
-    lookup = para_collec.add_lookup_parameters((len(word_dict), 45))
+    lookup = para_collec.add_lookup_parameters((features_total, 45), dy.NormalInitializer())
 
     w1 = dy.parameter(pW1)
     bias1 = dy.parameter(pBias1)
@@ -190,12 +190,43 @@ def main():
 
     #for index in range(0, len(train_feature_matrix)):
     for index in range(0, 100):
-        x = dy.inputTensor(train_feature_matrix[index])
+        # x = dy.inputTensor(train_feature_matrix[index])
         y_content = dy.scalarInput(train_content_label_set[index])
         y_type = dy.scalarInput(train_type_label_set[index])
         
-        
-        e_in = dy.sum_elems(x)/features_total
+        input_text = []
+        line = train_text_set[index].split()
+        for word in line:
+            # check if RT
+            if word == "RT":
+                input_text.append(lookup[len(word_dict)])
+            # check if hashtag
+            if word[0] == "#":
+                input_text.append(lookup[len(word_dict) + 1])
+
+            # check if mention
+            if word[0] == "@":
+                input_text.append(lookup[len(word_dict) + 2])
+
+            # just word itself 
+            if word in word_dict:
+                input_text.append(lookup[word_dict[word]])
+
+            try: 
+                # lower capiticalization of the word
+                lower_word = str(word).lower()
+                if lower_word in word_dict:
+                    input_text.append(lookup[word_dict[lower_word]])
+                # no punctuation 
+                replace_punctuation = str(word).maketrans(string.punctuation, '')
+                clean_word = str(word).translate(replace_punctuation)
+
+                if clean_word in word_dict:
+                    input_text.append(lookup[word_dict[clean_word]])
+            except:
+                continue
+
+        e_in = dy.sum_elems(input_text)/features_total
         e_affin1 = dy.affine_transform([bias1, w1, e_in])
         e_affin1 = rectify(e_affin1)
         e_content_affin2 = dy.affine_transform([bias2_content, w2_content, e_affin1])
@@ -206,23 +237,56 @@ def main():
         type_loss = type_output.scalar_value()
         
         if (index > 0) and (index % 100 == 0):
-    	    print("content_loss: ", content_loss, "type_loss", type_loss)
-    	
+            print("content_loss: ", content_loss, "type_loss", type_loss)
+        
         content_loss.backward()
-    	trainer.update()
-    	type_loss.backward()
-    	trainer.update()
+        trainer.update()
+        type_loss.backward()
+        trainer.update()
 
-    	dy.cg_checkpoint()
+        dy.cg_checkpoint()
 
     print("testing...")
     pred_content = []
     pred_type = []
     for index in range(0, len(test_feature_matrix)):
-    	x = dy.inputTensor(test_feature_matrix[index])
+        # x = dy.inputTensor(test_feature_matrix[index])
         y_content = dy.scalarInput(test_content_label_set[index])
         y_type = dy.scalarInput(test_type_label_set[index])
-        e_in = dy.sum_elems(x)/features_total
+       
+        input_text = []
+        line = train_text_set[index].split()
+        for word in line:
+            # check if RT
+            if word == "RT":
+                input_text.append(lookup[len(word_dict)])
+            # check if hashtag
+            if word[0] == "#":
+                input_text.append(lookup[len(word_dict) + 1])
+
+            # check if mention
+            if word[0] == "@":
+                input_text.append(lookup[len(word_dict) + 2])
+
+            # just word itself 
+            if word in word_dict:
+                input_text.append(lookup[word_dict[word]])
+
+            try: 
+                # lower capiticalization of the word
+                lower_word = str(word).lower()
+                if lower_word in word_dict:
+                    input_text.append(lookup[word_dict[lower_word]])
+                # no punctuation 
+                replace_punctuation = str(word).maketrans(string.punctuation, '')
+                clean_word = str(word).translate(replace_punctuation)
+
+                if clean_word in word_dict:
+                    input_text.append(lookup[word_dict[clean_word]])
+            except:
+                continue
+
+        e_in = dy.sum_elems(input_text)/features_total
         e_affin1 = dy.affine_transform([bias1, w1, e_in])
         e_affin1 = rectify(e_affin1)
         e_content_affin2 = dy.affine_transform([bias2_content, w2_content, e_affin1])
@@ -234,18 +298,17 @@ def main():
         pred_type.append(type_output)
         # type_loss = type_output.scalar_value()
 
-	misclassification_content = 0
-	misclassification_type = 0
- 	for index in range(0, len(pred_content)):
- 	    if pred_content[index] != test_content_label_set[index]:
- 		misclassification_content += 1
- 	    if pred_type[index] != test_type_label_set[index]:
- 		misclassification_type += 1
- 	print("content acc: ", misclassification_content/len(pred_content))
- 	print("type acc: ", misclassification_type/len(pred_type))
+    misclassification_content = 0
+    misclassification_type = 0
+    for index in range(0, len(pred_content)):
+        if pred_content[index] != test_content_label_set[index]:
+        misclassification_content += 1
+        if pred_type[index] != test_type_label_set[index]:
+        misclassification_type += 1
+    print("content acc: ", misclassification_content/len(pred_content))
+    print("type acc: ", misclassification_type/len(pred_type))
 
 # ----------------------------------------------------------
 
 if __name__ == '__main__':
     main()
-
